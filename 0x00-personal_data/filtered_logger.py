d70f2ca22f7b69b58f542c51722ac49a3f30ab07
Gendozzzz <sunflowermaina@gmail.com>
2024-11-08 01:23:21 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,13 +6,18 @@ import re
 from typing import List
 
 
+patterns = {
+        'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+        'replace': lambda x: r'\g<field>={}'.format(x),
+        }
+
+
 def filter_datum(
         fields: List[str],
         redaction: str,
         message: str,
         separator: str
-) -> str:
+        ) -> str:
     """Obfuscates specified fields."""
-    pattern = f"({'|'.join(fields)})=[^;]+"
-    return re.sub(
-        pattern, lambda match: f"{match.group(1)}={redaction}", message)
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
