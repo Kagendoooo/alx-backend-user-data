03a18465310b6761246433d6eb16b65d0fb8d97a
Gendozzzz <sunflowermaina@gmail.com>
2024-11-08 01:13:19 +0300

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Returns the log message obfuscated
+"""
+import re
+from typing import List
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+) -> str:
+    """Obfuscates specified fields."""
+    pattern = f"({'|'.join(fields)})=[^;]+"
+    return re.sub(
+        pattern, lambda match: f"{match.group(1)}={redaction}", message)
