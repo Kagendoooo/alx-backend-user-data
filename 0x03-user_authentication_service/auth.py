de4773c0170a40870d2a90ca0093dc9774b3efbb
Gendozzzz <sunflowermaina@gmail.com>
2024-11-22 02:05:33 +0300

    Generate UUIDs

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,9 +12,7 @@ import iuud
 
 def _hash_password(password: str) -> bytes:
     """Takes in a passowrd and returns bytes"""
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def _generate_uuid(self) -> str:
