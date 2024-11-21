14c4b42c65f4b87d3890cf6e5c583a21a29d28b3
Gendozzzz <sunflowermaina@gmail.com>
2024-11-22 01:42:12 +0300

    Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,3 +31,13 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password.decode('utf-8'))
             return user
+
+    def valid_login(self, email, password):
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'),
+                              user.hashed_password.encode('utf-8')):
+                return True
+            return False
+        except Exception:
+            return False
