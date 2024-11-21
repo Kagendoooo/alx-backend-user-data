f264e00c1f8dd954450f3df14c6aae3ccf533c9a
Gendozzzz <sunflowermaina@gmail.com>
2024-11-22 00:23:11 +0300

    Create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,10 +40,10 @@ class DB:
         """
         user = User(email=email, hashed_password=hashed_password)
         try:
-            self._session.add(new_user)
+            self._session.add(user)
             self._session.commit()
         except Exception as e:
             print(f"Error adding new user to database: {e}")
             self._session.rollback()
             raise
-        return new_user
+        return user
