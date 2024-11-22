3df891f5c6a1ffdb23ba90627cf75ac9be469e34
Gendozzzz <sunflowermaina@gmail.com>
2024-11-22 03:35:27 +0300

    Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,9 +91,9 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
-            raise ValueError()
+            raise ValueError("Invalid reset token")
 
-        hashed_password = _hash_password(password).decode('utf-8')
+        new_hashed_password = _hash_password(password)
         self._db.update_user(
                 user.id,
                 hashed_password=hashed_password,
